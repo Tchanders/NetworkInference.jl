58053690c428d03e8bf4458408f0df4a103e8b8b
Tchanders <thalia.e.chan@googlemail.com>
2017-08-05 11:47:40 +0100

    Hard-code fewer things
    Allow choices of discretizer, estimator, number of bins,
    base, and delimiter

--- src/common.jl
+++ src/common.jl
@@ -6,14 +6,15 @@ immutable Gene
 end
 
 # Make a Gene from a data file line
-# TODO: allow choose discretizer?
-function Gene(line::AbstractArray)
+# discretzed_values is expression_values mapped to bin ids, following discretization
+function Gene(line::AbstractArray, discretizer, estimator, number_of_bins)
 
     name = string(line[1])
     expression_values = Array{Float64}(line[2:end])
     discretized_values = zeros(Int, length(expression_values))
-    number_of_bins = get_bin_ids!(expression_values, "bayesian_blocks", 10, discretized_values)
-    probabilities = get_probabilities("maximum_likelihood", get_frequencies_from_bin_ids(discretized_values, number_of_bins))
+    # If discretizer is Bayesian blocks, number_of_bins will be overwritten
+    number_of_bins = get_bin_ids!(expression_values, discretizer, number_of_bins, discretized_values)
+    probabilities = get_probabilities(estimator, get_frequencies_from_bin_ids(discretized_values, number_of_bins))
 
     return Gene(name, discretized_values, number_of_bins, probabilities)
 
@@ -40,23 +41,32 @@ immutable NetworkAnalysis
     edges::Array{Edge} # Edges in descending order of confidence
 end
 
-# TODO: Allow choose discretizer?
-# TODO: Allow choose estimator?
-function get_genes(data_file_path::String)
+# The maximum likelihood estimator is recommended for PUC and PIDC, because speedups
+# are made here, based on the assumption that the marginal probability distribution for
+# a gene, from the joint distribution with any two other genes is always the same. If
+# the joint distributions are estimated using other estimators, this assumption is
+# violated for PUC and PIDC in get_puc and get_joint_probabilities.
+function get_genes(data_file_path::String; delim::Union{Char,Bool} = false, discretizer = "bayesian_blocks",
+    estimator = "maximum_likelihood", number_of_bins = 10)
 
-    lines = readdlm(open(data_file_path), skipstart = 1) # Assumes the first line is headers
+    if delim == false
+        lines = readdlm(open(data_file_path); skipstart = 1) # Assumes the first line is headers
+    else
+        lines = readdlm(open(data_file_path), delim; skipstart = 1) # Assumes the first line is headers
+    end
     number_of_genes = size(lines, 1)
     genes = Array{Gene}(number_of_genes)
 
     for i in 1:number_of_genes
-        genes[i] = Gene(lines[i:i, 1:end])
+        genes[i] = Gene(lines[i:i, 1:end], discretizer, estimator, number_of_bins)
     end
 
     return genes
 
 end
 
-# Networks are assumed to be undirected for now: edges are written in both directions with the same confidence
+# Networks are assumed to be undirected for now: edges are written in both directions
+# with the same confidence
 function write_network_file(file_name::String, network_analysis::NetworkAnalysis)
 
     out_file = open(file_name, "w")
--- src/network_inference.jl
+++ src/network_inference.jl
@@ -20,24 +20,28 @@ get_puc(::PIDCNetworkInference) = true
 # For sorting the edges
 get_confidence(edge::Edge) = edge.confidence
 
-function get_joint_probabilities(gene1, gene2)
+function get_joint_probabilities(gene1, gene2, estimator)
     frequencies = get_frequencies_from_bin_ids(
         gene1.discretized_values,
         gene2.discretized_values,
         gene1.number_of_bins,
         gene2.number_of_bins
     )
-    probabilities = get_probabilities("maximum_likelihood", frequencies)
-    # probabilities is already property of a gene, but doing this gets correct array shapes
+    probabilities = get_probabilities(estimator, frequencies)
+    # probabilities is already property of a gene, but doing this gets correct array shapes.
+    # Also, for MI and CLR, it means that we don't assume that the marginal probabilities for
+    # a gene are always the same, no matter what the second gene is, meaning that we can use
+    # estimators other than maximum likelihood. (We still can't do this for PUC and PIDC,
+    # because we do make that assumption for 3-gene joint distributions, in get_puc.)
     probabilities1 = sum(probabilities, 2)
     probabilities2 = sum(probabilities, 1)
     return (probabilities, probabilities1, probabilities2)
 end
 
-function get_mi_scores(genes, number_of_genes, base)
+function get_mi_scores(genes, number_of_genes, estimator, base)
 
     function get_mi(gene1, gene2, i, j, base, mi_scores)
-        probabilities, probabilities1, probabilities2 = get_joint_probabilities(gene1, gene2)
+        probabilities, probabilities1, probabilities2 = get_joint_probabilities(gene1, gene2, estimator)
         mi = apply_mutual_information_formula(probabilities, probabilities1, probabilities2, base)
         mi_scores[i, j] = mi
         mi_scores[j, i] = mi
@@ -53,10 +57,10 @@ function get_mi_scores(genes, number_of_genes, base)
 
 end
 
-function get_puc_scores(genes, number_of_genes, base)
+function get_puc_scores(genes, number_of_genes, estimator, base)
 
     function get_mi_and_si(gene1, gene2, base) # Mutual information and specific information
-        probabilities, probabilities1, probabilities2 = get_joint_probabilities(gene1, gene2)
+        probabilities, probabilities1, probabilities2 = get_joint_probabilities(gene1, gene2, estimator)
         mi = apply_mutual_information_formula(probabilities, probabilities1, probabilities2, base)
         si1 = apply_specific_information_formula(probabilities, probabilities1, probabilities2, 1, base)
         si2 = apply_specific_information_formula(probabilities, probabilities2, probabilities1, 2, base)
@@ -107,56 +111,66 @@ function get_puc_scores(genes, number_of_genes, base)
 
 end
 
-function get_confidences(inference, scores, number_of_genes)
+function get_confidences(inference, scores, number_of_genes, genes)
 
     # In their respective original implementations, CLR and PIDC applied network context in slightly
     # different ways. Those differences are respected here; in informal tests, they have not been
     # found to make much of a difference.
-    function get_confidence(::PIDCNetworkInference, i, j, scores, confidences)
+    function get_confidence(::PIDCNetworkInference, i, j, scores, confidences, genes)
         score = scores[i, j]
         scores_i = vcat(scores[1:i-1, i], scores[i+1:end, i])
         scores_j = vcat(scores[1:j-1, j], scores[j+1:end, j])
-        confidences[i, j] = cdf(fit(Gamma, scores_i), score) + cdf(fit(Gamma, scores_j), score)
+        try
+            confidences[i, j] = cdf(fit(Gamma, scores_i), score) + cdf(fit(Gamma, scores_j), score)
+        catch
+            println(string("Gamma distribution failed for ", genes[i].name, " and ", genes[j].name, "; used normal instead."))
+            get_clr_confidence(i, j, score, scores_i, scores_j, confidences)
+        end
     end
-    function get_confidence(::CLRNetworkInference, i, j, scores, confidences)
+    function get_confidence(::CLRNetworkInference, i, j, scores, confidences, genes)
         score = scores[i, j]
         scores_i = vcat(scores[1:i-1, i], scores[i+1:end, i])
         scores_j = vcat(scores[1:j-1, j], scores[j+1:end, j])
+        get_clr_confidence(i, j, score, scores_i, scores_j, confidences)
+    end
+    function get_clr_confidence(i, j, score, scores_i, scores_j, confidences)
         confidences[i, j] = sqrt(
-            (score - mean(scores_i))^2 / var(scores_i) +
-            (score - mean(scores_j))^2 / var(scores_j)
+            (var(scores_i) == 0 ? 0 : (score - mean(scores_i))^2 / var(scores_i)) +
+            (var(scores_j) == 0 ? 0 : (score - mean(scores_j))^2 / var(scores_j))
         )
     end
 
     confidences = SharedArray{Float64}(number_of_genes, number_of_genes)
     @sync @parallel for i in 1 : number_of_genes
         for j in i+1 : number_of_genes
-            get_confidence(inference, i, j, scores, confidences)
+            get_confidence(inference, i, j, scores, confidences, genes)
         end
     end
     return confidences
 
 end
 
-# TODO: allow choose estimator?
-# TODO: allow choose distribution?
-function NetworkAnalysis(inference::AbstractNetworkInference, genes::Array{Gene}; print_status = false)
+# The maximum likelihood estimator is recommended for PUC and PIDC, because speedups
+# are made here, based on the assumption that the marginal probability distribution for
+# a gene, from the joint distribution with any two other genes is always the same. If
+# the joint distributions are estimated using other estimators, this assumption is
+# violated for PUC and PIDC in get_puc and get_joint_probabilities.
+function NetworkAnalysis(inference::AbstractNetworkInference, genes::Array{Gene}; estimator = "maximum_likelihood", base = 2)
 
     # Constants and containers
-    base = 2
     number_of_genes = length(genes)
-    edges = Array{Edge}(binomial(number_of_genes, 2))
+    edges = Array{Edge}(binomial(number_of_genes, base))
 
     # Get the raw scores
     if get_puc(inference)
-        scores = get_puc_scores(genes, number_of_genes, base)
+        scores = get_puc_scores(genes, number_of_genes, estimator, base)
     else
-        scores = get_mi_scores(genes, number_of_genes, base)
+        scores = get_mi_scores(genes, number_of_genes, estimator, base)
     end
 
     # Apply context if necessary
     if apply_context(inference)
-        confidences = get_confidences(inference, scores, number_of_genes)
+        confidences = get_confidences(inference, scores, number_of_genes, genes)
     else
         confidences = scores
     end
