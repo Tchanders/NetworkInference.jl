8fb869ff3bb572662de12c8683d4e9ed0eec210e
Tchanders <thalia.e.chan@googlemail.com>
2017-09-22 13:15:55 +0100

    Make naming scheme more generic
    Avoid the use of specifically biological terms. Also improve on
    some of the more ambiguous names. This involves quite a few API
    changes:
    
    Gene -> Node
    Gene.name -> Node.label
    Gene.discretized_values -> Node.binned_values
    Edge.genes -> Edge.nodes
    Edge.confidence -> Edge.weight
    NetworkAnalysis -> InferredNetwork
    NetworkAnalysis.genes -> InferredNetwork.edges
    get_genes -> get_nodes
    
    infer_network
    * now returns an InferredNetwork
    * keyword argument out_file is now out_file_path

--- src/common.jl
+++ src/common.jl
@@ -1,32 +1,53 @@
-immutable Gene
-    name::String
-    discretized_values::Array{Int64}
+# Basic types for inferring a network
+
+# Node type with metadata:
+# - label: unique identifying label
+# - binned_values: data values discretized into bins
+# - number_of_bins: no. bins the data were discretized into
+# - probabilities: probability distribution across the bins
+struct Node
+    label::String
+    binned_values::Array{Int64}
     number_of_bins::Int64
     probabilities::Array{Float64}
 end
 
-# Make a Gene from a data file line
-# discretzed_values is expression_values mapped to bin ids, following discretization
-function Gene(line::AbstractArray, discretizer, estimator, number_of_bins)
+# Constructs a Node from a line of a data file. line should be an array with
+# the label as the first element, then the raw data values.
+function Node(line::AbstractArray, discretizer, estimator, number_of_bins)
 
-    name = string(line[1])
-    expression_values = Array{Float64}(line[2:end])
-    discretized_values = zeros(Int, length(expression_values))
-    # If discretizer is Bayesian blocks, number_of_bins will be overwritten
-    number_of_bins = get_bin_ids!(expression_values, discretizer, number_of_bins, discretized_values)
-    probabilities = get_probabilities(estimator, get_frequencies_from_bin_ids(discretized_values, number_of_bins))
+    label = string(line[1])
+    raw_values = Array{Float64}(line[2:end])
 
-    return Gene(name, discretized_values, number_of_bins, probabilities)
+    # Raw values are mapped to their bin IDs
+    binned_values = zeros(Int, length(raw_values))
+
+    # If the discretizer is Bayesian blocks, number_of_bins will be
+    # overwritten by the ideal number of bins. Otherwise, it will remain
+    # the same as the value passed in.
+    number_of_bins = get_bin_ids!(raw_values, discretizer, number_of_bins, binned_values)
+
+    probabilities = get_probabilities(estimator, get_frequencies_from_bin_ids(binned_values, number_of_bins))
+
+    return Node(label, binned_values, number_of_bins, probabilities)
 
 end
 
-immutable GenePair
-    mi::Float64 # Mutual information
-    si::Array{Float64} # Specific information
+# Type for caching information between pairs of nodes:
+# - mi: mutual information
+# - si: specific information
+struct NodePair
+    mi::Float64
+    si::Array{Float64}
 end
 
-# Networks are assumed to be undirected for now: order of the genes is arbitrary
-immutable Edge
-    genes::Array{Gene}
-    confidence::Float64
+# Edge type, undirected:
+# - nodes: the two nodes, in an arbitrary order
+# - weight: weight indicating confidence of edge existing in the true network
+# Weights are used to rank the edges, and different algorithms may have a
+# different scale. The relative weights within one inferred network are
+# therefore more meaningful than the absolute weight out of context.
+struct Edge
+    nodes::Array{Node}
+    weight::Float64
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,14 @@ using Base.Test
 # GeneNetWeaver: In silico benchmark generation and performance profiling of network inference methods.
 # Schaffter T, Marbach D, and Floreano D. Bioinformatics, 27(16):2263-70, 2011.
 
-println("Getting genes...")
-genes = get_genes("data/yeast1_10_data.txt")
+println("Getting nodes...")
+nodes = get_nodes("data/yeast1_10_data.txt")
 
 println("Inferring networks...")
-mi_network = NetworkAnalysis(MINetworkInference(), genes)
-clr_network = NetworkAnalysis(CLRNetworkInference(), genes)
-puc_network = NetworkAnalysis(PUCNetworkInference(), genes)
-pidc_network = NetworkAnalysis(PIDCNetworkInference(), genes)
+mi_network = InferredNetwork(MINetworkInference(), nodes)
+clr_network = InferredNetwork(CLRNetworkInference(), nodes)
+puc_network = InferredNetwork(PUCNetworkInference(), nodes)
+pidc_network = InferredNetwork(PIDCNetworkInference(), nodes)
 
 # The benchmarks were inferred prior to the implementation of this package. MI, PUC and PIDC were
 # inferred using scripts based on InformationMeasures.jl and CLR was inferred using MINET:
@@ -25,12 +25,12 @@ pidc_benchmark = readdlm("data/pidc.txt")
 
 # Compare a few selected edges throughout the inferred network
 for i in (1, 5, 10, 20, 40)
-    @test mi_network.edges[i].confidence ≈ mi_benchmark[2*i, 3] atol = 0.0001
+    @test mi_network.edges[i].weight ≈ mi_benchmark[2*i, 3] atol = 0.0001
     println("MI network inference $i passed")
-    @test clr_network.edges[i].confidence ≈ clr_benchmark[2*i, 3] atol = 0.0001
+    @test clr_network.edges[i].weight ≈ clr_benchmark[2*i, 3] atol = 0.0001
     println("CLR network inference $i passed")
-    @test puc_network.edges[i].confidence ≈ puc_benchmark[2*i, 3] atol = 0.0001
+    @test puc_network.edges[i].weight ≈ puc_benchmark[2*i, 3] atol = 0.0001
     println("PUC network inference $i passed")
-    @test pidc_network.edges[i].confidence ≈ pidc_benchmark[2*i, 3] atol = 0.0001
+    @test pidc_network.edges[i].weight ≈ pidc_benchmark[2*i, 3] atol = 0.0001
     println("PIDC network inference $i passed")
 end
