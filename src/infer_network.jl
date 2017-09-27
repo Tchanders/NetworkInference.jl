9972b120a1d6f08ad2ef2e356d80d1623f943221
Tchanders <thalia.e.chan@googlemail.com>
2017-09-27 14:15:00 +0100

    Make docstrings for exported functions

--- src/infer_network.jl
+++ src/infer_network.jl
@@ -1,18 +1,25 @@
 # Helper functions for inferring a network from a data file
 
-# Gets an array of all Nodes from a data file. It is assumed that the first
-# line of the file is headers (which are discarded) and the subsequent lines
-# each represent one node, and are of the form:
-# Label    data_value1  data_value2 ...
-# although a different delimiter may be specified.
-#
-# Keyword arguments:
-# - delim: the file's delimiter. Leave as false if it is whitespace
-# - discretizer: algorithm for discretizing the data
-# - estimator: algorithm for estimating the probability distribution
-#   "maximum_likelihood" is recommended for PUC and PIDC (see InferredNetwork
-#   type for more details)
-# - number_of_bins: will be overwritten if using "bayesian_blocks"
+"""
+    get_nodes(data_file_path::String; <keyword arguments>)
+
+Gets an array of all Nodes from a data file. It is assumed that the first
+line of the file is headers (which are discarded) and the subsequent lines
+each represent one node, and are of the form:
+
+Label    data_value1  data_value2 ...
+
+though a different delimiter may be specified.
+
+Arguments:
+* `data_file_path`: path to the data file
+* `delim=false`: the file's delimiter. Leave as false if it is whitespace
+* `discretizer="bayesian_blocks"`: algorithm for discretizing the data
+* `estimator="maximum_likelihood"`: algorithm for estimating probabilities
+* `number_of_bins=10`: will be overwritten if using "bayesian_blocks"
+
+The "maximum_likelihood" estimator is recommended for PUC and PIDC.
+"""
 function get_nodes(data_file_path::String; delim::Union{Char,Bool} = false, discretizer = "bayesian_blocks",
     estimator = "maximum_likelihood", number_of_bins = 10)
 
@@ -32,13 +39,25 @@ function get_nodes(data_file_path::String; delim::Union{Char,Bool} = false, disc
 
 end
 
-# Writes a network file from an InferredNetwork type. Each line of the file
-# will contain an edge, and since networks are assumed undirected, each edge
-# will be written in both directions with the same weight:
-# ...
-# LabelX   LabelY  WeightXY
-# LabelY   LabelX  WeightXY
-# ...
+"""
+    write_network_file(file_path::String, inferred_network::InferredNetwork)
+
+Writes a network file from an InferredNetwork type. Each line of the file
+will contain an edge, and since networks are assumed undirected, each edge
+will be written in both directions with the same weight:
+
+...
+
+LabelX   LabelY  WeightXY
+
+LabelY   LabelX  WeightXY
+
+...
+
+Arguments:
+* `file_path`: path to the output file
+* `inferred_network`: network that was inferred
+"""
 function write_network_file(file_path::String, inferred_network::InferredNetwork)
 
     out_file = open(file_path, "w")
@@ -57,12 +76,19 @@ function write_network_file(file_path::String, inferred_network::InferredNetwork
 
 end
 
-# Gets an adjacency matrix given an InferredNetwork and a threshold.
-#
-# Keyword arguments:
-# - absolute: interpret threshold as an absolute confidence score. If false,
-# threshold will be interpreted as the percentage of edges to keep.
-function get_adjacency_matrix(inferred_network::InferredNetwork, threshold = 1.0; absolute = false)
+"""
+    get_adjacency_matrix(inferred_network::InferredNetwork, threshold = 1.0; <keyword arguments>)
+
+Gets an adjacency matrix given an InferredNetwork and a threshold.
+
+Arguments:
+* `inferred_network`: network that was inferred
+* `threshold=0.1`: threshold above which to keep edges in the network
+* `absolute=false`: interpret threshold as an absolute confidence score
+
+If `absolute` is false, threshold will be interpreted as the percentage of edges to keep.
+"""
+function get_adjacency_matrix(inferred_network::InferredNetwork, threshold = 0.1; absolute = false)
 
     number_of_nodes = length(inferred_network.nodes)
     adjacency_matrix = zeros(Bool, (number_of_nodes, number_of_nodes))
@@ -91,23 +117,30 @@ function get_adjacency_matrix(inferred_network::InferredNetwork, threshold = 1.0
 
 end
 
-# Infers a network, given a data file and a network inference algorithm. It
-# is assumed that the first line of the file is headers (which are
-# discarded) and the subsequent lines each represent one node, and are of
-# the form:
-# Label    data_value1  data_value2 ...
-# although a different delimiter may be specified.
-#
-# Keyword arguments:
-# - delim: the file's delimiter. Leave as false if it is whitespace
-# - discretizer: algorithm for discretizing the data
-# - estimator: algorithm for estimating the probability distribution
-#   "maximum_likelihood" is recommended for PUC and PIDC (see InferredNetwork
-#   type for more details)
-# - number_of_bins: will be overwritten if using "bayesian_blocks"
-# - base: base for the information measures
-# - out_file_path: path to output file. If left as empty string, no file will
-# be written.
+"""
+    infer_network(data_file_path::String, inference::AbstractNetworkInference; <keyword arguments>)
+
+Infers a network, given a data file and a network inference algorithm. It
+is assumed that the first line of the file is headers (which are
+discarded) and the subsequent lines each represent one node, and are of
+the form:
+
+Label    data_value1  data_value2 ...
+
+though a different delimiter may be specified.
+
+Arguments:
+* `data_file_path`: path to the data file
+* `inference`: network inference algorithm (e.g. `PIDCNetworkInference()`)
+* `delim=false`: the file's delimiter. Leave as false if it is whitespace
+* `discretizer="bayesian_blocks"`: algorithm for discretizing the data
+* `estimator="maximum_likelihood"`: algorithm for estimating probabilities
+* `number_of_bins=10`: will be overwritten if using "bayesian_blocks"
+* `base=2`: base for the information measures
+* `out_file_path=""`: path to output file. If empty, will not write a file
+
+The "maximum_likelihood" estimator is recommended for PUC and PIDC.
+"""
 function infer_network(data_file_path::String, inference::AbstractNetworkInference; delim::Union{Char,Bool} = false,
     discretizer = "bayesian_blocks", estimator = "maximum_likelihood", number_of_bins = 10, base = 2,
     out_file_path = "")
